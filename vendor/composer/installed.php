0889c05ba15015d6ad6b3b4954bc1d8cfa1de30c
andreslopezuni <andres.lopez.m@uniminuto.edu>
2026-04-23 15:27:42 -0500

    Se agregan cambios para quitar el JWT

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'ecc48ff44c109c5f49d74e3ce46187dcef669f17',
+        'reference' => '787e084bc43f106aa45f8212a121a3f8fa8736f7',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'ecc48ff44c109c5f49d74e3ce46187dcef669f17',
+            'reference' => '787e084bc43f106aa45f8212a121a3f8fa8736f7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
