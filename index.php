8aae406e5078011376e39bd2c16aec012cbbee77
andreslopezuni <andres.lopez.m@uniminuto.edu>
2026-04-23 16:28:04 -0500

    Cambios de headers

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, PATCH, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+//header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Methods: GET, PATCH, POST, PUT, DELETE, OPTIONS");
+//header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json');
 require 'vendor/autoload.php';
 require 'middleware/authMiddleware.php';
